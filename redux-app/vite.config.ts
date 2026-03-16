105f9e2b91967ec29af616c021def36944ebf15d
Pratyush Kesarwani <96560994+pk170970@users.noreply.github.com>
2026-03-16 04:27:25 +0000

    vite configuration changes

--- redux-app/vite.config.ts
+++ redux-app/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Redux-fundamentals/' //added for deployment in github pages
 })
